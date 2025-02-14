758fc0e86b307d539642ad344aef6e16769fde80
hieu <hieu@hieus-MacBook-Pro.local>
2025-02-15 00:21:11 +0700

    create core

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,3 +4,13 @@ export const LIST_MOVIES = '/movie/popular?api_key=YOUR_API_KEY&language=vi-VN&p
 export const FILTER_MOVIE = '/discover/movie?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28'
 export const DETAIL_MOVIE = '/movie/{movie_id}?api_key=YOUR_API_KEY&language=vi-VN'
 
+
+//  khai bao key api 
+// export const FILTER_MOVIE = 'https://api.themoviedb.org/3/discover/movie?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28'
+
+//
+// URL_BASE =https://api.themoviedb.org/3
+//  KEY_LINK  =/discover/movie
+// param , body url ==>?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28
+
+// 
