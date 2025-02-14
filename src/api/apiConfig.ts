30d55b3279684fb82b5a50499b36795807ec3ec4
duy08112005 <vanduy08112005@gmail.com>
2025-02-15 00:00:21 +0700

    Init Project

---
+++ src/api/apiConfig.ts
@@ -0,0 +1,6 @@
+export const URL_BASE = 'https://api.themoviedb.org/3'
+export const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+export const LIST_MOVIES = '/movie/popular?api_key=YOUR_API_KEY&language=vi-VN&page=1'
+export const FILTER_MOVIE = '/discover/movie?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28'
+export const DETAIL_MOVIE = '/movie/{movie_id}?api_key=YOUR_API_KEY&language=vi-VN'
+
