f75f59d1ebb3a74ded48f3c40f2aa1937ed93e02
hieu <hieu@hieus-MacBook-Pro.local>
2025-02-15 00:23:15 +0700

    create core

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,8 +1,8 @@
 export const URL_BASE = 'https://api.themoviedb.org/3'
 export const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-export const LIST_MOVIES = '/movie/popular?api_key=YOUR_API_KEY&language=vi-VN&page=1'
-export const FILTER_MOVIE = '/discover/movie?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28'
-export const DETAIL_MOVIE = '/movie/{movie_id}?api_key=YOUR_API_KEY&language=vi-VN'
+export const LIST_MOVIES = '/movie/popular?api_key=0e33d55cf2dc8cf4c0364babc3b52354&language=vi-VN&page=1'
+export const FILTER_MOVIE = '/discover/movie?api_key=0e33d55cf2dc8cf4c0364babc3b52354&language=vi-VN&with_genres=28'
+export const DETAIL_MOVIE = '/movie/{movie_id}?api_key=0e33d55cf2dc8cf4c0364babc3b52354&language=vi-VN'
 
 
 //  khai bao key api 
@@ -13,4 +13,4 @@ export const DETAIL_MOVIE = '/movie/{movie_id}?api_key=YOUR_API_KEY&language=vi-
 //  KEY_LINK  =/discover/movie
 // param , body url ==>?api_key=YOUR_API_KEY&language=vi-VN&with_genres=28
 
-// 
+// URL_BASE+LIST_MOVIES
